6cd98c706c87aa0b862b07ba6c4a2ecab9a93b84
Mostafa-Tawfik <m8ustafa.tawfik@gmail.com>
2021-12-20 08:13:54 +0200

    feat: add animations when moving from page to page
    add animations when moving from page to page,
    
    add animations when clicking on my name to go home page,
    
    add animation to the menu button,
    
    modify the social icon animation to go horizontally in about and portfolio pages,
    
    fix responsive on mobile devices,
    
    update the preview of the landing page project.

--- app.js
+++ app.js
@@ -5,16 +5,37 @@
 
 const menuToggle = document.querySelector('.toggle');
 const nextToggle = document.querySelector('.next');
+const previousToggle = document.querySelector('.before');
+const homeToggle = document.querySelector('.home');
 const showcase = document.querySelector('.showcase');
 const social = document.querySelector('.social.vertical');
+const logo = document.querySelector('.logo');
 const menu = document.querySelector('.menu');
 const btn = document.querySelector('.upBtn');
+
+
+// delay moving to next page
+
+function delay (URL) {
+    setTimeout( ()=> { window.location = URL }, 1500 );
+}
 
 /**
  * End Global Variables
  * Begin Main Functions
  * 
 */
+
+// scroll to top button
+// hide the button
+
+window.onscroll= function () {
+    this.scrollY >= 300 ?btn.classList.add('show'):btn.classList.remove('show');
+}
+// scroll to top
+function goUP() {
+    window.scrollTo({top:0, behavior:"smooth"});
+}
 
 menuToggle.addEventListener('click', ()=>{
     menuToggle.classList.toggle('active');
@@ -23,22 +44,29 @@ menuToggle.addEventListener('click', ()=>{
 });
 
 
+homeToggle.addEventListener('click', ()=>{
+    homeToggle.classList.toggle('homePage');
+    logo.classList.toggle('homePage');
+});
+
 nextToggle.addEventListener('click', ()=>{
     nextToggle.classList.toggle('nextPage');
     showcase.classList.toggle('nextPage');
     menu.classList.toggle('nextPage');
+    social.classList.toggle('nextPage');
+});
+
+previousToggle.addEventListener('click', ()=>{
+    previousToggle.classList.toggle('previousPage');
+    showcase.classList.toggle('previousPage');
+    menu.classList.toggle('previousPage');
+    social.classList.toggle('previousPage');
 });
 
 
-// scroll to top button
-// hide the button
-window.onscroll= function () {
-    this.scrollY >= 300 ?btn.classList.add('show'):btn.classList.remove('show');
-}
-// scroll to top
-function goUP() {
-    window.scrollTo({top:0, behavior:"smooth"});
-}
+
+
+
 
 
 /**
