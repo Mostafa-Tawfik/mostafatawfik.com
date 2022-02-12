74787449b2699a5540c46e79057dce35e15cdd23
Mostafa-Tawfik <m8ustafa.tawfik@gmail.com>
2022-02-12 13:26:55 +0200

    init next.js app

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default function about() {
+  return <div>
+    <h1>about</h1>
+  </div>;
+}
--- src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return <div className='footer'>
-    <img className='footer-logo' alt='myLogo' src='../../images/logo.jpg'></img>
-    <p>Copyright <span>Mostafa Tawfik</span> © 2022</p>
-  </div>;
-}
