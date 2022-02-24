ed59de4a1b738f7c14dbb144f44510942c69c10b
Mostafa-Tawfik <m8ustafa.tawfik@gmail.com>
2022-02-24 11:46:35 +0200

    change the cv link to a google drive

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import DisplayLottie from '../components/DisplayLottie';
-import development from '../public/Lottie/development.json'
 import { motion } from 'framer-motion';
 import styles from '../styles/About.module.scss'
 import Education from '../components/Education';
@@ -34,26 +32,31 @@ export default function about() {
       </div>
       
       <section className={styles['about-layout']} id='about'>
+
         <section className={styles['about-header']}>
-        <div className={styles['about-header-desc']}>
-          <h1>Hi .. I'm Mostafa</h1>
-          <p>A Front-End web developer and UI/UX designer well-versed in Scrum and Agile with experience in building and designing static and dynamic responsive web applications with JavaScript / React / Next.js / SASS / Tailwind and other libraries like Framer Motion and Lottie etc ... </p>
-          <br></br>
-          <p>I build meaningful, usable yet joyful user experiences with a user-centered design approach.</p>
-          <br></br>
-          <p>Enhancing online content in order to improve the quality and quantity of website traffic and improve the website's position in search results pages.</p> 
-          <button onClick={() => {
-            window.open("../Mostafa-Tawfik-Resume.pdf", "_blank")}} className={styles.myResume}>My Resume</button>
-        </div>
+
+          <div className={styles['about-header-desc']}>
+            <h1>Hi .. I'm Mostafa</h1>
+            <p>A Front-End web developer and UI/UX designer well-versed in Scrum and Agile with experience in building and designing static and dynamic responsive web applications with JavaScript / React / Next.js / SASS / Tailwind and other libraries like Framer Motion and Lottie etc ... </p>
+            <br></br>
+            <p>I build meaningful, usable yet joyful user experiences with a user-centered design approach.</p>
+            <br></br>
+            <p>Enhancing online content in order to improve the quality and quantity of website traffic and improve the website's position in search results pages.</p> 
+            <button className={styles.myResume}><a 
+          href="https://drive.google.com/file/d/1xex8xtvZenebgfbiFi7nyVcFMIgfwgcA/view?usp=sharing" 
+          target="_blank "
+          rel="noreferrer">
+          My Resume</a></button>
+          </div>
+
           <Carousel />
 
-          {/* <DisplayLottie lottiePath= {development} height={400} /> */}
         </section>
-        
-        
+                
         <Services />
         <MyStack />
         <Education />
+
       </section>
 
     </motion.div>
